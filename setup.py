8f763599fecc13502f1e71c3a6731ad206537f9d
ftc9533mm <ftc9533mm@gmail.com>
2018-03-03 11:33:47 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='directpy',
-      version='0.2',
+      version='0.2.1',
       description='A python library for interacting with DirecTV receivers',
-      url='http://github.com/sentry07/DirectPy',
-      author='Eric Walters',
+      url='http://github.com/madikasu/DirectPy',
+      author='Doug Miller',
       license='MIT',
       py_modules=['DirectPy'])
